adc64ee59f2f7a6813072df9b8b6905bc1876045
saitarun <saitarun@blindmatrix.co.uk>
2024-06-14 19:38:21 +0530

    Test Suit added

--- BinPackAlgo.php
+++ BinPackAlgo.php
@@ -1,5 +1,6 @@
 <?php
 
+const BEST_FIT = 'best_fit';
 /*
 |--------------------------------------------------------------------------
 | Bin Packing Algo
@@ -9,7 +10,7 @@
 | $itemsToFit accepts how many garbage bags with their sizes.
 */
     
-function binPackAlgo($binData, $itemsToFit, $algoType = 'best_fit')
+function binPackAlgo($binData, $itemsToFit, $algoType = BEST_FIT)
 {
     if($algoType == 'best_fit')
     {
@@ -35,6 +36,7 @@ function binPackAlgo($binData, $itemsToFit, $algoType = 'best_fit')
                         break;
                 }
             }
+            $bin['remainingBin'] = $remaining_bin_space;
             $bin['fitData'] = $fitItems;
         }
         $binData['unFitPieces'] = $itemsToFit;
@@ -54,17 +56,28 @@ $binData = [
     ['binSize' => 22, 'name' => 'H'],
     ['binSize' => 12, 'name' => 'I'],
     ['binSize' => 14, 'name' => 'J'],
+    ['binSize' => 28, 'name' => 'K'],
+    ['binSize' => 35, 'name' => 'L'],
+    ['binSize' => 19, 'name' => 'M'],
+    ['binSize' => 25, 'name' => 'N'],
+    ['binSize' => 40, 'name' => 'O'],
+    ['binSize' => 22, 'name' => 'P'],
+    ['binSize' => 18, 'name' => 'Q'],
+    ['binSize' => 29, 'name' => 'R'],
+    ['binSize' => 33, 'name' => 'S'],
+    ['binSize' => 17, 'name' => 'T'],
 ];
 
 $itemsToFit = [
-    ['size' => 8],
+    ['size' => 8.5],
     ['size' => 3],
+    ['size' => 1.5],
     ['size' => 6],
     ['size' => 15],
     ['size' => 9],
     ['size' => 4],
-    ['size' => 10],
-    ['size' => 12],
+    ['size' => 100],
+    ['size' => 9.57],
     ['size' => 5],
     ['size' => 7],
     ['size' => 14],
@@ -82,6 +95,31 @@ $itemsToFit = [
     ['size' => 23],
     ['size' => 24],
     ['size' => 25],
+    ['size' => 8.75],
+    ['size' => 3.5],
+    ['size' => 2.25],
+    ['size' => 6.5],
+    ['size' => 15.25],
+    ['size' => 9.5],
+    ['size' => 0.5],
+    ['size' => 4.75],
+    ['size' => 10.5],
+    ['size' => 11.5],
+    ['size' => 5.5],
+    ['size' => 7.5],
+    ['size' => 14.5],
+    ['size' => 1.75],
+    ['size' => 13.5],
+    ['size' => 17.5],
+    ['size' => 19.5],
+    ['size' => 20.5],
+    ['size' => 21.5],
+    ['size' => 16.5],
+    ['size' => 18.5],
+    ['size' => 22.5],
+    ['size' => 23.5],
+    ['size' => 24.5],
+    ['size' => 25.5],
 ];
 
 
